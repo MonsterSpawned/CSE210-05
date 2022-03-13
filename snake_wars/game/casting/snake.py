08b43dbe9282521069a44f7bbedcf2aef38fe66b
TheModdersDen <monsterspawned@gmail.com>
2022-03-12 21:08:34 -0700

    Removed more old files. Fixed player positions.

--- snake_wars/game/casting/snake.py
+++ snake_wars/game/casting/snake.py
@@ -60,10 +60,10 @@ class Snake(Actor):
     
     def _prepare_body(self):
         if self._player_num == 1:
-            x = int(self._data.MAX_X / 10)
+            x = int(self._data.MAX_X / 3)
             y = int(self._data.MAX_Y / 2)
         elif self._player_num == 2:
-            x = int(self._data.MAX_X / 5)
+            x = int(self._data.MAX_X / 1.75)
             y = int(self._data.MAX_Y / 2)
 
         for i in range(self._data.SNAKE_LENGTH):
